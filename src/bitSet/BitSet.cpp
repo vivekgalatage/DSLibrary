ffc646880d59bf89bdb7e8493541ee938301179a
Amogh Bihani <amogh@hcl.(none)>
2013-10-13 12:47:43 +0530

    Changes made according to review

--- src/bitSet/BitSet.cpp
+++ src/bitSet/BitSet.cpp
@@ -2,30 +2,29 @@
 
 BitSet::BitSet(int size)
 {
-	m_size = size/32 + 1;
-    for (int i = 0; i < m_size; ++i)
-        m_array.push_back(0);
+    m_sizeOfInt = sizeof(int);
+    m_size = size/m_sizeOfInt + 1;
+    m_array[m_size];
 }
 
 int BitSet::valueAt(int pos) const
 {
-	int bucket = 0;
-	int localPosition = 0;
-	bucket = pos/32;
-	localPosition = pos%32;	
-	int ans, temp;
-    //std::cout << "bucket" << bucket << std::endl;
-	temp = m_array[bucket];
+    int bucket = 0;
+    int localPosition = 0;
+    bucket = pos/m_sizeOfInt;
+    localPosition = pos%m_sizeOfInt;	
+    int ans, temp;
+    temp = m_array[bucket];
     ans = temp >> localPosition & 1;
-	return ans;
+    return ans;
 }
 
 void BitSet::setValueAt(const int pos, const int value)
 {
-	int bucket = 0;
-	int localPosition = 0;
-	bucket = pos/32;
-	localPosition = pos%32;
+    int bucket = 0;
+    int localPosition = 0;
+    bucket = pos/m_sizeOfInt;
+    localPosition = pos%m_sizeOfInt;
     if (value)
         m_array[bucket] = m_array[bucket] | (1 << localPosition);
     else
@@ -36,14 +35,3 @@ int BitSet::operator[](const int pos) const
 {
     return valueAt(pos);
 }
-
-/*int main()
-{
-    BitSet bitSet(3);
-    bitSet.setValueAt(0, 1);
-    bitSet.setValueAt(1, 0);
-    bitSet.setValueAt(2, 1);
-    std::cout << bitSet[0] << std::endl;
-    std::cout << bitSet[1] << std::endl;
-    std::cout << bitSet[2] << std::endl;
-}*/
--- src/bitSet/BitSet.h
+++ src/bitSet/BitSet.h
@@ -1,13 +1,16 @@
-#include<iostream>
-#include<vector>
+#ifndef BITSET_H
+#define BITSET_H
 
 class BitSet {
 public:
-	BitSet(int);
-	int valueAt(int) const;
-	void setValueAt(const int, const int);
-    int operator[](const int);
+    BitSet(int);
+    int valueAt(int) const;
+    void setValueAt(const int, const int);
+    int operator[](const int) const;
+
 private:
-	int m_size;
-	std::vector<int> m_array;
+    int m_size;
+    int m_array[];
+    int m_sizeOfInt;
 };
+#endif
