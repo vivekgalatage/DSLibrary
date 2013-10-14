d0932dff049b977d7c9f47d845837c3dba026f41
Vivek Galatage <vivekgalatage@gmail.com>
2013-10-15 00:37:36 +0530

    Simplifying BitSet operations

--- src/utils/BitSet.cpp
+++ src/utils/BitSet.cpp
@@ -11,32 +11,23 @@ BitSet::BitSet(int size)
 
 bool BitSet::test(int bitPosition) const
 {
-    int bucket = 0;
-    int localPosition = 0;
-    bucket = bitPosition / sizeof(int);
-    localPosition = bitPosition % sizeof(int);	
-    int ans = 0, temp = 0;
-    temp = m_array[bucket];
-    ans = temp >> localPosition & 1;
-    return ans;
+    int bucket = bitPosition / sizeof(int);
+    int localPosition = bitPosition % sizeof(int);
+    return (m_array[bucket] >> localPosition) & 1;
 }
 
 void BitSet::set(const int bitPosition)
 {
-    int bucket = 0;
-    int localPosition = 0;
-    bucket = bitPosition / sizeof(int);
-    localPosition = bitPosition % sizeof(int); 
+    int bucket = bitPosition / sizeof(int);
+    int localPosition = bitPosition % sizeof(int);
     m_array[bucket] = m_array[bucket] | (1 << localPosition);
 }
 
 void BitSet::reset(const int bitPosition)
 {
-    int bucket = 0;
-    int localPosition = 0;
-    bucket = bitPosition / sizeof(int);
-    localPosition = bitPosition % sizeof(int); 
-    m_array[bucket] = m_array[bucket] & ~(1 << localPosition);   
+    int bucket = bitPosition / sizeof(int);
+    int localPosition = bitPosition % sizeof(int);
+    m_array[bucket] = m_array[bucket] & ~(1 << localPosition);
 }
 
 bool BitSet::operator[](const int bitPosition) const
