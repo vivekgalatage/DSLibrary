4eb66c077e31c48bc45d0c96da980adb6df3efa5
Vivek Galatage <vivekgalatage@gmail.com>
2013-10-22 00:25:36 +0530

    Using more intuitive ~0 instead of -1 to set/check all the bits

--- src/utils/BitSet.cpp
+++ src/utils/BitSet.cpp
@@ -10,7 +10,7 @@ BitSet::BitSet(int size, bool setAllBits)
         ++m_size;
     m_array = new unsigned int[m_size];
     for (int i = 0; i < m_size; ++i)
-        m_array[i] = (setAllBits ? -1 : 0);
+        m_array[i] = (setAllBits ? ~0 : 0);
 }
 
 BitSet::~BitSet()
@@ -47,7 +47,7 @@ bool BitSet::operator[](const int bitPosition) const
 bool BitSet::all() const 
 {
     for (int i = 0; i < m_size; ++i) {
-        if (static_cast<int>(m_array[i]) != -1)
+        if (static_cast<int>(m_array[i]) != ~0)
             return false;
     }
     return true;
