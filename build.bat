5f45bf501ed482d38ef8812dd157a85a34203351
Vivek Galatage <vivekgalatage@gmail.com>
2013-10-24 02:40:32 +0530

    Testing solution and vcxproj generation using gyp on AppVeyor server

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 git submodule init
 git submodule update
 set PATH=C:\python27;%PATH%
-python --version
+where python
+third-party\gyp\gyp --depth=. DSLibrary.gyp
 msbuild %MSBuildLogger%\my-msbuild-log.xml DSLibrary.sln
 Debug\dslibrary.exe
