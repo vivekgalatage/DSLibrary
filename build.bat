a1bae5beceeb683f8aa2fad5bb9eac3459e96830
Vivek Galatage <vivekgalatage@gmail.com>
2013-10-24 08:32:35 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
 git submodule init
 git submodule update
-set PATH=C:\python27;.\third-party\gyp;%PATH%
 where python
 REM third-party\gyp\gyp.bat --depth=. DSLibrary.gyp
-gyp --depth=. -G msvs_version=2010e DSLibrary.gyp
+.\third-party\gyp\gyp --depth=. -G msvs_version=2010e DSLibrary.gyp
 msbuild DSLibrary.sln
 Debug\dslibrary.exe
